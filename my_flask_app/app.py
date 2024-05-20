15c31ad2cf01bd5dd4ab01e55967834d6801dfb3
Zach <Zach@Zanzibars-Computer.local>
2024-05-20 12:17:56 -0700

    python takeoff

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/second')
+def second_page():
+    return render_template('second_page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
